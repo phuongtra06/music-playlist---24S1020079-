ee7f8ab77a47e05fe0009d24aceb306e224410d7
phuongtra06 <nguyenphuongtra171206@gmail.com>
2025-12-02 08:14:38 +0700

    feat:taokhungct

---
+++ playlistt.py
@@ -0,0 +1,26 @@
+songs = []
+
+def main():
+    while True:
+        print("\n--- MUSIC PLAYLIST MANAGER ---")
+        print("1. Thêm bài hát")
+        print("2. Xem danh sách phát")
+        print("3. Tìm bài hát theo ca sĩ")
+        print("4. Thoát")
+
+        choice = input("Chọn chức năng: ")
+
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            print("Kết thúc chương trình.")
+            break
+        else:
+            print("Lựa chọn không hợp lệ.")
+
+if __name__ == "__main__":
+    main()
